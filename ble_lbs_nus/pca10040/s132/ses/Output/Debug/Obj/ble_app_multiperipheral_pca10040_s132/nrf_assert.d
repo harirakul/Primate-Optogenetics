87174f5c9511404781d9c6df8b3d948d239816da
[Heonmin <hm1113j@gmail.com>
2023-03-18 09:17:57 -0700

    Integrated lbs and nus service

---
+++ ble_lbs_nus/pca10040/s132/ses/Output/Debug/Obj/ble_app_multiperipheral_pca10040_s132/nrf_assert.d
@@ -0,0 +1,27 @@
+Output/Debug/Obj/ble_app_multiperipheral_pca10040_s132/nrf_assert.o: \
+ C:\Users\admin\Desktop\research\DeviceDownload\nRF5SDK1500a53641a\nRF5_SDK_15.0.0_a53641a\components\libraries\util\nrf_assert.c \
+ C:\Users\admin\Desktop\research\DeviceDownload\nRF5SDK1500a53641a\nRF5_SDK_15.0.0_a53641a\components\libraries\util\nrf_assert.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/__SEGGER_RTL_Arm_Conf.h \
+ ../../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:\Users\admin\Desktop\research\DeviceDownload\nRF5SDK1500a53641a\nRF5_SDK_15.0.0_a53641a\components\libraries\util\app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stdio.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.10a/include/stdbool.h \
+ C:\Users\admin\Desktop\research\DeviceDownload\nRF5SDK1500a53641a\nRF5_SDK_15.0.0_a53641a\components\libraries\util\sdk_errors.h \
+ ../../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ C:\Users\admin\Desktop\research\DeviceDownload\nRF5SDK1500a53641a\nRF5_SDK_15.0.0_a53641a\components\libraries\util\nordic_common.h \
+ C:\Users\admin\Desktop\research\DeviceDownload\nRF5SDK1500a53641a\nRF5_SDK_15.0.0_a53641a\components\libraries\util\app_error_weak.h
